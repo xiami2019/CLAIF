51c492e025455bdc23c1b92e068b852b7db8f79f
xiami2019 <chengqy2019@foxmail.com>
2023-08-13 12:59:44 +0800

    add inference, model list and evaluation on sts

--- README.md
+++ README.md
@@ -69,5 +69,72 @@ python run_training.py \
 ### CLHAIF
 Coming soon.
 
-## Evaluation
-Coming soon.
+## Inference 
+### Model List
+Our released models are listed as following. You can import these models by using [Sentence Transformers](https://github.com/UKPLab/sentence-transformers) or using [HuggingFace's Transformers](https://github.com/huggingface/transformers). 
+|              Model              | Avg. STS |
+|:-------------------------------|:--------:|
+|  [fnlp/claif-bert-base](https://huggingface.co/fnlp/claif-bert-base) |   79.63 |
+| [fnlp/claif-roberta-base](https://huggingface.co/fnlp/claif-roberta-base) |   79.90  |
+|    [fnlp/claif-scaled-bert-base](https://huggingface.co/fnlp/claif-scaled-bert-base)    |   82.37  |
+|    [fnlp/claif-scaled-roberta-base](https://huggingface.co/fnlp/claif-scaled-roberta-base)   |   81.88  |
+
+#### Use CLAIF with Sentence-Transformers
+```python
+from sentence_transformers import SentenceTransformer
+sentences = ["This is an example sentence", "Each sentence is converted"]
+
+model = SentenceTransformer('fnlp/claif-scaled-bert-base')
+embeddings = model.encode(sentences)
+print(embeddings)
+
+```
+
+#### Use CLAIF with HuggingFace Transformers
+```python
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+
+#Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+
+# Sentences we want sentence embeddings for
+sentences = ['This is an example sentence', 'Each sentence is converted']
+
+# Load model from HuggingFace Hub
+tokenizer = AutoTokenizer.from_pretrained('fnlp/claif-scaled-bert-base')
+model = AutoModel.from_pretrained('fnlp/claif-scaled-bert-base')
+
+# Tokenize sentences
+encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+
+# Compute token embeddings
+with torch.no_grad():
+    model_output = model(**encoded_input)
+
+# Perform pooling. In this case, mean pooling.
+sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+
+print("Sentence embeddings:")
+print(sentence_embeddings)
+```
+
+### Evaluation of CLAIF
+You can run the evaluation script for claif like:
+```python
+python evaluation_sts.py --model_name_or_path 'fnlp/claif-roberta-base' --mode test --task_set sts
+```
+which is expected to output the results in a tubular format:
+```
+------ test ------
++-------+-------+-------+-------+-------+--------------+-----------------+-------+
+| STS12 | STS13 | STS14 | STS15 | STS16 | STSBenchmark | SICKRelatedness |  Avg. |
++-------+-------+-------+-------+-------+--------------+-----------------+-------+
+| 68.33 | 82.26 | 77.00 | 85.18 | 83.43 |    85.05     |      78.02      | 79.90 |
++-------+-------+-------+-------+-------+--------------+-----------------+-------+
+```
---
+++ evaluation_sts.py
@@ -0,0 +1,179 @@
+import sys
+import logging
+import argparse
+from sentence_transformers import SentenceTransformer, util, models
+from sentence_transformers.cross_encoder import CrossEncoder
+from sentence_transformers.cross_encoder.evaluation import CECorrelationEvaluator
+
+from prettytable import PrettyTable
+
+# Set up logger
+# logging.basicConfig(format='%(asctime)s : %(message)s', level=logging.DEBUG)
+
+# Set PATHs
+PATH_TO_SENTEVAL = './SentEval'
+PATH_TO_DATA = './SentEval/data'
+# Import SentEval
+sys.path.insert(0, PATH_TO_SENTEVAL)
+import senteval
+
+class AdaEmbeddings:
+    def __init__(self, api_key) -> None:
+        pass
+
+def print_table(task_names, scores):
+    tb = PrettyTable()
+    tb.field_names = task_names
+    tb.add_row(scores)
+    print(tb)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name_or_path", type=str, 
+            help="Sentence-Transformers' model name or path")
+    parser.add_argument("--model_type", type=str, default='bi', choices=['cross', 'bi', 'ada'])
+    parser.add_argument("--mode", type=str, 
+            choices=['dev', 'test', 'fasttest'],
+            default='test', 
+            help="What evaluation mode to use (dev: fast mode, dev results; test: full mode, test results); fasttest: fast mode, test results")
+    parser.add_argument("--task_set", type=str, 
+            choices=['sts', 'transfer', 'full', 'na'],
+            default='sts',
+            help="What set of tasks to evaluate on. If not 'na', this will override '--tasks'")
+    parser.add_argument("--tasks", type=str, nargs='+', 
+            default=['STS12', 'STS13', 'STS14', 'STS15', 'STS16',
+                     'MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC',
+                     'SICKRelatedness', 'STSBenchmark'], 
+            help="Tasks to evaluate on. If '--task_set' is specified, this will be overridden")
+
+    args = parser.parse_args()
+
+    # Load transfomers' model checkpoint
+    if args.model_type == 'bi':
+        model = SentenceTransformer(args.model_name_or_path)
+    elif args.model_type == 'cross':
+        model = CrossEncoder(args.model_name_or_path)
+    elif args.model_type == 'ada':
+        model = AdaEmbeddings()
+
+    # Set up the tasks
+    if args.task_set == 'sts':
+        args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
+    elif args.task_set == 'transfer':
+        args.tasks = ['MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC']
+    elif args.task_set == 'full':
+        args.tasks = ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']
+        args.tasks += ['MR', 'CR', 'MPQA', 'SUBJ', 'SST2', 'TREC', 'MRPC']
+
+    # Set params for SentEval
+    if args.mode == 'dev' or args.mode == 'fasttest':
+        # Fast mode
+        params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 5}
+        params['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
+                                         'tenacity': 3, 'epoch_size': 2}
+    elif args.mode == 'test':
+        # Full mode
+        params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 10}
+        params['classifier'] = {'nhid': 0, 'optim': 'adam', 'batch_size': 64,
+                                         'tenacity': 5, 'epoch_size': 4}
+    else:
+        raise NotImplementedError
+
+    # SentEval prepare and batcher
+    def prepare(params, samples):
+        return
+
+    def batcher_for_cross(params, batch1, batch2, max_length=None):
+        # Handle rare token encoding issues in the dataset
+        if len(batch1) >= 1 and len(batch1[0]) >= 1 and isinstance(batch1[0][0], bytes):
+            batch1 = [[word.decode('utf-8') for word in s] for s in batch1]
+
+        if len(batch2) >= 1 and len(batch2[0]) >= 1 and isinstance(batch2[0][0], bytes):
+            batch2 = [[word.decode('utf-8') for word in s] for s in batch2]
+
+        sentences1 = [' '.join(s) for s in batch1]
+        sentences2 = [' '.join(s) for s in batch2]
+        eval_pair = [[x[0], x[1]] for x in zip(sentences1, sentences2)]
+        scores = model.predict(eval_pair)
+        return scores.tolist()
+
+    def batcher(params, batch, max_length=None):
+        # Handle rare token encoding issues in the dataset
+        if len(batch) >= 1 and len(batch[0]) >= 1 and isinstance(batch[0][0], bytes):
+            batch = [[word.decode('utf-8') for word in s] for s in batch]
+
+        sentences = [' '.join(s) for s in batch]
+        return model.encode(sentences, convert_to_tensor=True).cpu()
+
+    results = {}
+
+    for task in args.tasks:
+        if args.model_type == 'bi':
+            se = senteval.engine.SE(params, batcher, prepare)
+            result = se.eval(task)
+        elif args.model_type == 'cross':
+            se = senteval.engine.SE(params, batcher_for_cross, prepare)
+            result = se.eval_cross(task)
+        results[task] = result
+
+    # Print evaluation results
+    if args.mode == 'dev':
+        print("------ %s ------" % (args.mode))
+
+        task_names = []
+        scores = []
+        for task in ['STSBenchmark', 'SICKRelatedness']:
+            task_names.append(task)
+            if task in results:
+                scores.append("%.2f" % (results[task]['dev']['spearman'][0] * 100))
+            else:
+                scores.append("0.00")
+        print_table(task_names, scores)
+
+        task_names = []
+        scores = []
+        for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
+            task_names.append(task)
+            if task in results:
+                scores.append("%.2f" % (results[task]['devacc']))    
+            else:
+                scores.append("0.00")
+        task_names.append("Avg.")
+        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
+        print_table(task_names, scores)
+
+    elif args.mode == 'test' or args.mode == 'fasttest':
+        print("------ %s ------" % (args.mode))
+
+        task_names = []
+        scores = []
+        for task in ['STS12', 'STS13', 'STS14', 'STS15', 'STS16', 'STSBenchmark', 'SICKRelatedness']:
+            task_names.append(task)
+            if task in results:
+                if task in ['STS12', 'STS13', 'STS14', 'STS15', 'STS16']:
+                    scores.append("%.2f" % (results[task]['all']['spearman']['all'] * 100))
+                else:
+                    scores.append("%.2f" % (results[task]['test']['spearman'].correlation * 100))
+            else:
+                scores.append("0.00")
+        task_names.append("Avg.")
+        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
+        print_table(task_names, scores)
+
+        task_names = []
+        scores = []
+        for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
+            task_names.append(task)
+            if task in results:
+                scores.append("%.2f" % (results[task]['acc']))    
+            else:
+                scores.append("0.00")
+        task_names.append("Avg.")
+        scores.append("%.2f" % (sum([float(score) for score in scores]) / len(scores)))
+        print_table(task_names, scores)
+
+
+if __name__ == "__main__":
+    main()
+
+    
